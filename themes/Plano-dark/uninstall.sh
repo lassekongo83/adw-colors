3bd827b3f62cbac0ca03f4f78b3d6b423254f83d
lassekongo83 <lassekongo83@users.noreply.github.com>
2023-04-28 08:23:40 +0000

    Shellchecked

--- themes/Plano-dark/uninstall.sh
+++ themes/Plano-dark/uninstall.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
-gtk3_file="$HOME/.config/gtk-3.0/gtk.css"
-gtk4_file="$HOME/.config/gtk-4.0/gtk.css"
+gtk3_file="${HOME}/.config/gtk-3.0/gtk.css"
+gtk4_file="${HOME}/.config/gtk-4.0/gtk.css"
 
 if [ -L "${gtk3_file}" ]; then
   unlink "${gtk3_file}"
@@ -16,11 +16,11 @@ else
   exit 1
 fi
 
-if [ -L $HOME/.config/gtk-3.0/assets ]; then
-  unlink $HOME/.config/gtk-3.0/assets
+if [ -L "${HOME}"/.config/gtk-3.0/assets ]; then
+  unlink "${HOME}"/.config/gtk-3.0/assets
 fi
-if [ -L $HOME/.config/gtk-4.0/assets ]; then
-  unlink $HOME/.config/gtk-4.0/assets
+if [ -L "${HOME}"/.config/gtk-4.0/assets ]; then
+  unlink "${HOME}"/.config/gtk-4.0/assets
 fi
 
 echo "adw-colors: Theme uninstalled. If there were any previous created bak file you can restore it now if needed."
